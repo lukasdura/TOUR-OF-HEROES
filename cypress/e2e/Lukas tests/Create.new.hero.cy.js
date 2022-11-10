52aab26f20916c8b1f10c45ac7ce75b3639d51eb
lukasdura <xswatx@gmail.com>
2022-11-10 16:50:00 +0100

    new tests created

---
+++ cypress/e2e/Lukas tests/Create.new.hero.cy.js
@@ -0,0 +1,22 @@
+
+// <reference types="cypress" />
+
+
+describe('Creating new hero', ()=>{
+
+
+    it('', ()=>{
+
+
+        cy.visit('http://localhost:4200/heroes')
+         cy.get('#new-hero')
+          .click('')
+           .type('Spiderman {enter}')
+           
+         
+           cy.get('.add-button')
+            .click('')
+            
+
+    })
+})
---
+++ cypress/e2e/Lukas tests/Delete.hero.cy.js
@@ -0,0 +1,18 @@
+
+// <reference types="cypress" />
+
+
+describe('Creating new hero', ()=>{
+
+
+    it('', ()=>{
+
+
+        cy.visit('http://localhost:4200/heroes')
+         cy.get(':nth-child(9) > .delete')
+         .click()
+
+
+
+    })
+    })
